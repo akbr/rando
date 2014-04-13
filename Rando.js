45615f785d27e9d677ee6ca7955782383739ddf5
Aaron <akbr@akbr.net>
2014-04-13 18:28:52 -0400

    first commit

---
+++ Rando.js
@@ -0,0 +1,172 @@
+//  Rando.js
+//  c/o Aaron Rieke
+//  Special thanks to Johannes Baagøe (for Alea, the PRNG that does all the work) and Victor Quinn (for the wrapper idea)
+//  MIT licensed; have at it.
+
+(function () {
+  var randoMethods, Rando,
+    Mash, Alea;
+
+  randoMethods = {
+    // random: [implemented by Alea -- like Math.random],
+
+    between: function (min, max) {
+      if (max == null) {
+        max = min;
+        min = 0;
+      }
+      return min + Math.floor(this.random() * (max - min + 1));
+    },
+
+    normal: function (options) {
+      options = options || {};
+      options.mean = options.mean || 0;
+      options.sdev = options.sdev || 1;
+
+      var num = this.normalPair()[0] * options.sdev + options.mean;
+
+      var result;
+
+      if (options.min === undefined && options.max === undefined) {
+        result = num;
+      } else if (num > options.max) {
+        result = options.max;
+      } else if (num < options.min) {
+        result = options.min;
+      } else {
+        result = num;
+      }
+
+      return result;
+    },
+
+    normalPair: function () {
+      // Implements the Box-Mueller transform
+      // http://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+      var x = 0, y = 0, rds, c;
+
+      // Get two random numbers from -1 to 1.
+      // If the radius is zero or greater than 1, throw them out and pick two new ones
+      // Rejection sampling throws away about 20% of the pairs.
+      do {
+        x = this.random()*2-1;
+        y = this.random()*2-1;
+        rds = x*x + y*y;
+      }
+      while (rds == 0 || rds > 1)
+
+      // This magic is the Box-Muller Transform
+      c = Math.sqrt(-2*Math.log(rds)/rds);
+
+      return [x*c, y*c];
+    }
+  };
+
+  Rando = function (seeds) {
+    return (function (args) {
+      var newRando = Object.create(randoMethods);
+      newRando.random = Alea.apply(this, args);
+      return newRando;
+    }(Array.prototype.slice.call(arguments)));
+  };
+
+  // Johannes Baagøe <baagoe@baagoe.com>, 2010
+  // From http://baagoe.com/en/RandomMusings/javascript/
+  Mash = function () {
+    var n = 0xefc8249d;
+
+    var mash = function(data) {
+      data = data.toString();
+      for (var i = 0; i < data.length; i++) {
+        n += data.charCodeAt(i);
+        var h = 0.02519603282416938 * n;
+        n = h >>> 0;
+        h -= n;
+        h *= n;
+        n = h >>> 0;
+        h -= n;
+        n += h * 0x100000000; // 2^32
+      }
+      return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+    };
+
+    mash.version = 'Mash 0.9';
+    return mash;
+  }
+
+  Alea = function () {
+    return (function(args) {
+      var s0 = 0;
+      var s1 = 0;
+      var s2 = 0;
+      var c = 1;
+
+      if (args.length == 0) {
+        args = [+new Date];
+      }
+      var mash = Mash();
+      s0 = mash(' ');
+      s1 = mash(' ');
+      s2 = mash(' ');
+
+      for (var i = 0; i < args.length; i++) {
+        s0 -= mash(args[i]);
+        if (s0 < 0) {
+          s0 += 1;
+        }
+        s1 -= mash(args[i]);
+        if (s1 < 0) {
+          s1 += 1;
+        }
+        s2 -= mash(args[i]);
+        if (s2 < 0) {
+          s2 += 1;
+        }
+      }
+      mash = null;
+
+      var random = function() {
+        var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
+        s0 = s1;
+        s1 = s2;
+        return s2 = t - (c = t | 0);
+      };
+      random.uint32 = function() {
+        return random() * 0x100000000; // 2^32
+      };
+      random.fract53 = function() {
+        return random() + 
+          (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
+      };
+      random.version = 'Alea 0.9';
+      random.args = args;
+      return random;
+
+    } (Array.prototype.slice.call(arguments)));
+  };
+
+  // ------------
+  // Exports, etc.
+  // ------------
+
+  // CommonJS module
+  if (typeof exports !== 'undefined') {
+      if (typeof module !== 'undefined' && module.exports) {
+          exports = module.exports = Rando;
+      }
+      exports.Rando = Rando;
+  }
+
+  // Register as a named AMD module
+  if (typeof define === 'function' && define.amd) {
+      define([], function () {
+          return Rando;
+      });
+  }
+
+  // If there is a window object, that at least has a document property,
+  // stick in the global scope
+  if (typeof window === "object" && typeof window.document === "object") {
+      window.Rando = Rando;
+  }
+})();
